4ce0e039e83b560859800ce550205dc2384be346
Awxccc <sancturez@gmail.com>
2024-01-23 14:18:29 +0800

    Updated Unity Project

---
+++ 9_JingXun_Lab10/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScore(0);
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -20f, 20f), Mathf.Clamp(transform.position.y, -3.9f, 3.9f));
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene("GameOver");
+
+        }
+    }
+    public void UpdateScore(int value)
+    {
+        score += value;
+        scoreText.text = "Score : " + score;
+    }
+}
---
+++ 9_JingXun_Lab10/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject;
+    public GameObject[] SpawnObjects1;
+    public int random1;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        random1 = Random.Range(0, 3);
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObjects1[random1], transform.position, transform.rotation);
+    }
+}
